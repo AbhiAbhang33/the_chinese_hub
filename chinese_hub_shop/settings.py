b23ec94f893d1227c4dc93e1da31780048ea29dd
Abhi <abhangabhaishek99200@gmail.com>
2026-09-10 00:58:02 +0530

    Configure PostgreSQL database with dj-database-url

--- chinese_hub_shop/settings.py
+++ chinese_hub_shop/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = "chinese_hub_shop.wsgi.application"
 # =============================================================================
 
 DATABASES = {
-    "default": dj_database_url.config(
+    'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
         conn_health_checks=True,
