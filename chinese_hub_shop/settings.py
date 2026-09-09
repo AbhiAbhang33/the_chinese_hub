ca31de08c423b3a9e4b0b01d6d192b0286211afc
Abhi <abhangabhaishek99200@gmail.com>
2026-09-10 02:48:26 +0530

    Configure Cloudinary media storage

--- chinese_hub_shop/settings.py
+++ chinese_hub_shop/settings.py
@@ -253,7 +253,7 @@ OWNER_PHONE_TEL = "7757855545"
 # Country code +91 + number
 # No +, spaces, or hyphens
 
-OWNER_WHATSAPP_NUMBER = "917757855545"
+OWNER_WHATSAPP_NUMBER = "7757855545"
 
 
 # =============================================================================
